8c4b4cffd6e2cdc56250a4516af6898b796fd9c5
Carvi3 <andrewcarvajal38@gmail.com>
2023-12-21 15:02:25 -0500

    Initial commit

---
+++ src/main/java/com/carv/firstinit/FirstInitApplication.java
@@ -0,0 +1,13 @@
+package com.carv.firstinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carv/firstinit/FirstInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carv.firstinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
